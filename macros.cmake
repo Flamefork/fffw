bae613300efd3c829707ee45792038614e8f823e
Ilia Ablamonov <ilia@flamefork.ru>
2017-08-30 18:21:54 +0400

    Rework flash step

--- macros.cmake
+++ macros.cmake
@@ -8,10 +8,12 @@ macro(add_library_dependency target name)
     target_link_libraries(${target} ${name})
 endmacro(add_library_dependency)
 
-macro(create_hex name)
-    add_custom_command(TARGET ${name} POST_BUILD COMMAND avr-objcopy ARGS -O ihex -R.eeprom ${name} ${name}.hex)
-endmacro(create_hex)
-
-macro(flash_hex name device baud)
-    add_custom_command(TARGET ${name} POST_BUILD COMMAND c45b -d -b ${baud} -p ${device} -f ${name}.hex -r)
-endmacro(flash_hex)
+macro(flash name baud port)
+    add_custom_command(
+            TARGET ${name} POST_BUILD
+            COMMAND avr-strip ${name}
+            COMMAND avr-size --format=avr --mcu=atmega64 ${name}
+            COMMAND avr-objcopy -O ihex -R.eeprom ${name} ${name}.hex
+            COMMAND c45b -d -b ${baud} -p ${port} -f ${name}.hex -r
+    )
+endmacro(flash)
