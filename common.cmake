e176eeb6210426e213a50594aa9a9d80c82f3923
Ilia Ablamonov <ilia@flamefork.ru>
2017-08-30 11:40:14 +0400

    Add c45b flashing to CMake

--- common.cmake
+++ common.cmake
@@ -27,3 +27,7 @@ endmacro(add_project_library_dependency)
 macro(create_hex name)
     add_custom_command(TARGET ${name} POST_BUILD COMMAND avr-objcopy ARGS -O ihex -R.eeprom ${name} ${name}.hex)
 endmacro(create_hex)
+
+macro(flash_hex name device baud)
+    add_custom_command(TARGET ${name} POST_BUILD COMMAND c45b -d -b ${baud} -p ${device} -f ${name}.hex -r)
+endmacro(flash_hex)
